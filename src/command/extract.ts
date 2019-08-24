4b34c25a7ae01bae0d4a3b2f8f71bde4ef720877
Luku <luku.3659@gmail.com>
2019-08-24 15:22:37 +0900

    ty抽出処理ãひとまずå®実装

--- src/command/extract.ts
+++ src/command/extract.ts
@@ -4,8 +4,11 @@ import { Command } from 'commander'
 import { actionRunner } from '../lib/errorHandler'
 import commandInterFace from '../lib/commandInterFace'
 import { logger } from '../lib/logger'
-import inquirer from 'inquirer'
+import inquirer, { QuestionCollection } from 'inquirer'
 import { promisify } from 'util'
+import _ from 'lodash'
+
+const OUTPUT = 'output.json'
 
 type Args = {
     filePath: string
@@ -29,20 +32,18 @@ export default class Extract implements commandInterFace {
 
         logger.info(`filePath : ${this.filePath}`)
 
+        // Reading a file
         const buf = await promisify(fs.readFile)(this.filePath, {
             encoding: 'utf-8',
         }).catch(() => {
             throw new Error('The file could not be read.')
         })
+        const sideJson: side.Side = JSON.parse(buf)
+        const sideTests: side.Test[] = sideJson.tests
 
-        const side: side.Side = JSON.parse(buf)
-        let choices: string[] = []
+        const choices = _.map(sideTests, 'name')
 
-        side.tests.forEach(elem => {
-            choices.push(elem.name)
-        })
-
-        let questions = [
+        let questions: QuestionCollection = [
             {
                 type: 'list',
                 name: 'type',
@@ -51,15 +52,44 @@ export default class Extract implements commandInterFace {
             },
             {
                 type: 'list',
-                name: 'test',
+                name: 'name',
                 message: 'Please select a test.',
                 choices: choices,
             },
         ]
 
-        inquirer.prompt(questions).then(answers => {
-            logger.info(`answers: ${JSON.stringify(answers)}`)
+        const answers = await inquirer.prompt(questions)
+        logger.info(`answers: ${JSON.stringify(answers)}`)
+
+        const test = sideTests.find(test => test.name === answers.name)
+
+        let commands = test!.commands
+
+        if (!commands) {
+            logger.error(`test name: ${answers.name}`)
+            throw new Error('There is no test content.')
+        }
+
+        const result =
+            answers.type === 'data'
+                ? _.chain(commands)
+                      .takeWhile(command => command.comment !== 'main')
+                      .filter(command => command.comment !== 'data')
+                      .value()
+                : _.chain(commands)
+                      .dropWhile(command => command.comment !== 'main')
+                      .filter(command => command.comment !== 'main')
+                      .value()
+
+        // Writing to file
+        await promisify(fs.writeFile)(
+            OUTPUT,
+            JSON.stringify(result, null, '    ')
+        ).catch(err => {
+            throw new Error('Failed to write file')
         })
+
+        logger.info('Exported to output.json.')
     }
 
     // Perform optional validation
